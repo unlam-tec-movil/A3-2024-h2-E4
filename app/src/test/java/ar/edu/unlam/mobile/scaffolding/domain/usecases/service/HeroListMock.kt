14d746f509757dca99b78191a9e55f17c90eb455
Dangerous875 <crbarzabal@gmail.com>
2024-10-16 22:40:02 -0300

    test linters

--- app/src/test/java/ar/edu/unlam/mobile/scaffolding/domain/usecases/service/HeroListMock.kt
+++ app/src/test/java/ar/edu/unlam/mobile/scaffolding/domain/usecases/service/HeroListMock.kt
@@ -1,14 +1,44 @@
 package ar.edu.unlam.mobile.scaffolding.domain.usecases.service
 
+import ar.edu.unlam.mobile.scaffolding.data.local.SuperHeroItem
 import ar.edu.unlam.mobile.scaffolding.data.network.model.SuperHeroAppearance
 import ar.edu.unlam.mobile.scaffolding.data.network.model.SuperHeroBiography
 import ar.edu.unlam.mobile.scaffolding.data.network.model.SuperHeroImage
-import ar.edu.unlam.mobile.scaffolding.data.local.SuperHeroItem
 import ar.edu.unlam.mobile.scaffolding.data.network.model.SuperHeroPowerStats
 
 object HeroListMock {
+    val heroList =
+        listOf(
+            SuperHeroItem(
+                "1",
+                "Name",
+                SuperHeroPowerStats("50", "50", "50", "50", "50", "50"),
+                SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
+                SuperHeroAppearance("gender", "race"),
+                SuperHeroImage("URL"),
+                null,
+            ),
+            SuperHeroItem(
+                "2",
+                "Name",
+                SuperHeroPowerStats("50", "50", "50", "50", "50", "50"),
+                SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
+                SuperHeroAppearance("gender", "race"),
+                SuperHeroImage("URL"),
+                null,
+            ),
+            SuperHeroItem(
+                "3",
+                "Name",
+                SuperHeroPowerStats("50", "50", "50", "50", "50", "50"),
+                SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
+                SuperHeroAppearance("gender", "race"),
+                SuperHeroImage("URL"),
+                null,
+            ),
+        )
 
-    val heroList = listOf(
+    val heroGetMock =
         SuperHeroItem(
             "1",
             "Name",
@@ -16,35 +46,8 @@ object HeroListMock {
             SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
             SuperHeroAppearance("gender", "race"),
             SuperHeroImage("URL"),
-            null
-        ), SuperHeroItem(
-            "2",
-            "Name",
-            SuperHeroPowerStats("50", "50", "50", "50", "50", "50"),
-            SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
-            SuperHeroAppearance("gender", "race"),
-            SuperHeroImage("URL"),
-            null
-        ), SuperHeroItem(
-            "3",
-            "Name",
-            SuperHeroPowerStats("50", "50", "50", "50", "50", "50"),
-            SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
-            SuperHeroAppearance("gender", "race"),
-            SuperHeroImage("URL"),
-            null
+            null,
         )
-    )
 
-    val heroGetMock =  SuperHeroItem(
-        "1",
-        "Name",
-        SuperHeroPowerStats("50", "50", "50", "50", "50", "50"),
-        SuperHeroBiography("pepito", "pepito", "pepito", "pepito"),
-        SuperHeroAppearance("gender", "race"),
-        SuperHeroImage("URL"),
-        null
-    )
-
-    var heroSetMock : SuperHeroItem? = null
+    var heroSetMock: SuperHeroItem? = null
 }
