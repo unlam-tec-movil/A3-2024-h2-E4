2748c953f32d72baa074a9c8fb8f76ecd4ade8b8
Gabriel Pezet <gabriel.pzt@gmail.com>
2024-10-25 01:11:22 -0300

    Update User profile Screen, link UserAvatar with firebaseStorage

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/evolution/data/network/utils/Constants.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/evolution/data/network/utils/Constants.kt
@@ -1,10 +1,19 @@
 package ar.edu.unlam.mobile.scaffolding.evolution.data.network.utils
 
 object Constants {
-    const val IMAGES = "images"
+    const val IMAGES = "userAvatarImages"
     const val URL = "url"
     const val CREATED_AT = "createdAt"
-    const val UID = "avatar"
-    const val IMAGE_NAME = "$UID.jpg"
+    private val UID: String? = null
+    val IMAGE_NAME: String = "$UID.jpg"
     const val ALL_IMAGES = "image/*"
 }
+
+// object Constants {
+//    const val IMAGES = "images"
+//    const val URL = "url"
+//    const val CREATED_AT = "createdAt"
+//    const val UID = "avatar"
+//    const val IMAGE_NAME = "$UID.jpg"
+//    const val ALL_IMAGES = "image/*"
+// }
